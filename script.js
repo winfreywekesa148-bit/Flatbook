223b00a78f2bcc5148295ddd564196fb357412e6
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-16 07:27:35 +0300

    Set text content for book store title and list
    Update text content of book store title and book list.

---
+++ script.js
@@ -0,0 +1,10 @@
+const bookStoreTitle = document.querySelector('#header');
+ 
+// update
+bookStoreTitle.textContent = bookStoreTitle.name;
+
+const bookList = document.querySelector('book-list');
+
+// update
+bookList.textContent = bookList.name;
+
